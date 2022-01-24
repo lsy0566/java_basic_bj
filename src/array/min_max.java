fd3840d2a6f18933f016865b7de72d64da6d1524
ldw <lsy0566@naver.com>
2022-01-25 01:49:07 +0900

    min&max solve

---
+++ src/array/min_max.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.Scanner;
+
+public class min_max {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int[] a = new int[n];	// 배열의 길이를 먼저 설정하는 방법.
+		
+		for (int i = 0; i<n ; i++) {
+			int b = s.nextInt();
+			a[i] = b;
+		}
+		
+		int max = a[0];
+		int min = a[0];
+		
+		for (int j = 1; j < n; j++) {
+			if(a[j] > max) {
+				max = a[j];
+			}
+			if(a[j] < min) {
+				min = a[j];
+			}
+		}
+		
+		System.out.println(min + " " + max);
+		
+	}
+}
